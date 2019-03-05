4843785d4c964078c1c43e4e65b689200ea6ce00
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 22:43:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,8 +3,8 @@ def first_challenge
      :montague => {
 :patriarch => {:name =>"Lord Montague", :age =>"53"},
 :matriarch => {:name => "Lady Montague", :age => "54"},
-:hero => {}
-:hero_friends => {}
+:hero => {name: "Romeo", age: "15", status: "alive"},
+:hero_friends => []
    },
      :capulet => {
 :patriarch => {}
