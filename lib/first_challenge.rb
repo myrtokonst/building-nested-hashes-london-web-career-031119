158b9ef599c8fd41e02ea5d7dec57deee73046d0
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 22:43:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,8 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-:patriarch => {:name =>"Lord Montague", :age =>"53"}
-:matriarch => {:name => "Lady Montague", :age => "54"}
+:patriarch => {:name =>"Lord Montague", :age =>"53"},
+:matriarch => {:name => "Lady Montague", :age => "54"},
 :hero => {}
 :hero_friends => {}
    },
